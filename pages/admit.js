81c018e6799fab55cf8b083d0a7539e82000cf7d
julbly38-a11y <julbly38@gmail.com>
2026-06-10 15:59:42 +0300

    fix: прибрати дублікат import у admit.js

--- pages/admit.js
+++ pages/admit.js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
 import { createClient } from '../lib/supabase'
 
 const DEPARTMENTS = [
